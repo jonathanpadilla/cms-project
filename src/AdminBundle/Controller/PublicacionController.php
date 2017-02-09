05df409329a0708ce901ec3ca5a2de4fab2a3192
Jonathan Padilla <jonathanpadilla09@outlook.com>
2017-02-09 18:57:18 -0400

    administrar bloques

---
+++ src/AdminBundle/Controller/PublicacionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use AdminBundle\Entity\CmsBloque;
+
+class PublicacionController extends Controller
+{
+    public function indexAction($id = 0)
+    {
+    	// utils
+    	$default_data = $this->get('DefaultData');
+    	$default_data->setHeaderTitle('Administrar bloque');
+        $model_bloque = $this->get('ModelBloque');
+
+        // editar
+        $pagina = false;
+        if($id)
+        {
+            $em = $this->getDoctrine()->getManager();
+            $pagina = $em->getRepository('AdminBundle:CmsPagina')->findOneBy(['pagIdPk' => $id]);
+        }
+
+        $bloques = $this->renderView('AdminBundle:Layouts:bloque_admin.html.twig', ['bloques' => $model_bloque->getAll($id)]);
+
+        return $this->render('AdminBundle::pagina_bloques.html.twig', [
+            'pagina'        => $pagina,
+            'bloques'       => $bloques,
+        	'default_data' 	=> $default_data->getAll()
+        	]);
+    }
+}
